a307cef774690a718a575de30e4e3421318957ab
Parth Patil <parth253pp@outlook.com>
2025-02-16 17:48:58 +0530

    Init commit

---
+++ contactManager/src/main/java/com/contactManager/ContactManagerApplication.java
@@ -0,0 +1,12 @@
+package com.contactManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactManagerApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactManagerApplication.class, args);
+	}
+}
